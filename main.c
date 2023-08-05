76e5a849d338a19d717a484d9d7b2b939d7bb768
serpatsama <keetarpalkuhs@gmail.com>
2023-08-06 03:37:53 +0530

    Snaky boi now wraps around, he buggy tho Also collisions are buggy asw Changed up the color palette a bit, looks noice

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include "setup.h"
 
+uint64_t snake_size = 0;
+
 int main(void)
 {
     Initialize_game();
-
-    uint64_t snake_size = 0;
 
     InitWindow(WIDTH, HEIGHT, "Gaym");
     SetTargetFPS(60);
@@ -15,24 +15,18 @@ int main(void)
 
         SnakeControl(dt);
 
-        if(DetectRectCollision(snake.player, food.entity))
-        {
-            food.entity.x = GetRandomValue(0, BLOCK_COUNT - 1);
-            food.entity.y = GetRandomValue(0, BLOCK_COUNT - 1);
-            snake_size++;
-        }
+        //TODO: Switch to a better collision function
+        //NOTE: Maye AABB might help
+        CollisionFunctions();
 
-        if (IsKeyReleased(KEY_SPACE))
-        {
-            printf("Frame-Time: %lf\n", dt);
-        }
+        DebugFunctions();
 
         BeginDrawing();
-            ClearBackground(BLACK);
+            ClearBackground(SAGE);
 
             DrawRectangleRounded((Rectangle){(int)food.entity.x * BLOCK_SIZE, (int)food.entity.y * BLOCK_SIZE, food.entity.width, food.entity.height}, food.roundedness, 1, food.color);
 
-            DrawRectangleRec((Rectangle){(int)snake.player.x * BLOCK_SIZE, (int)snake.player.y * BLOCK_SIZE, snake.player.width, snake.player.height}, RED);
+            DrawRectangleRec((Rectangle){(int)snake.player.x * BLOCK_SIZE, (int)snake.player.y * BLOCK_SIZE, snake.player.width, snake.player.height}, REDISH);
 
         EndDrawing();
 
@@ -48,7 +42,7 @@ void Initialize_game(void)
     snake.player.height = BLOCK_SIZE;
     snake.player.width  = BLOCK_SIZE;
     snake.player.x = 5;
-    snake.player.y = 10;
+    snake.player.y = 14;
     snake.speed = 15.0f;
 
     food.entity.height = BLOCK_SIZE;
@@ -56,12 +50,7 @@ void Initialize_game(void)
     food.entity.x = 0;
     food.entity.y = 10;
     food.roundedness = 0.95;
-    food.color = LIME;
-}
-
-bool DetectRectCollision(Rect a, Rect b)
-{
-    return ((floor(a.x) == floor(b.x)) && (floor(a.y) == floor(b.y)));
+    food.color = ORANGISH;
 }
 
 void SnakeControl(double dt)
@@ -95,4 +84,58 @@ void SnakeControl(double dt)
     {
         snake.player.y += (snake.speed * snake.y_direction) * dt;
     }
+
+    WrapSnakeBody();
+}
+
+void CollisionFunctions(void)
+{
+    if(DetectRectCollision(snake.player, food.entity))
+    {
+        food.entity.x = 0;
+        food.entity.y = GetRandomValue(0, BLOCK_COUNT - 1);
+        snake_size++;
+
+        //dbg
+        printf("Snake {%lf, %lf}, FOOD {%lf, %lf}\n", snake.player.x, snake.player.y, food.entity.x, food.entity.y);
+    }
+
+}
+
+//NOTE This is a bit buggy rn, the snake head can change directions while off screen.
+void WrapSnakeBody(void)
+{
+    if (snake.player.x < 0 && snake.x_direction == -1)
+    {
+        snake.player.x += (BLOCK_COUNT + WRAP_DELAY);
+    }
+    else if (snake.player.x > BLOCK_COUNT && snake.x_direction == 1)
+    {
+        snake.player.x -= (BLOCK_COUNT + WRAP_DELAY);
+    }
+    
+    
+    if (snake.player.y < 0 && snake.y_direction == -1)
+    {
+        snake.player.y += (BLOCK_COUNT + WRAP_DELAY);
+    }
+    else if (snake.player.y > BLOCK_COUNT && snake.y_direction == 1)
+    {
+        snake.player.y -= (BLOCK_COUNT + WRAP_DELAY);
+    }
+    
+}
+
+bool DetectRectCollision(Rectangle a, Rectangle b)
+{
+    return ((floor(a.x) == floor(b.x)) && (floor(a.y) == floor(b.y)));
+}
+
+void DebugFunctions(void)
+{
+    if (IsKeyReleased(KEY_SPACE))
+    {
+        printf("Frame-Time: %lf\n", GetFrameTime());
+    }
+
 }
--- setup.h
+++ setup.h
@@ -7,20 +7,21 @@
 #define BLOCK_SIZE 40
 #define BLOCK_COUNT 25
 
+//Colors RGBA
+#define SAGE      CLITERAL(Color){0xCE, 0xCE, 0x5A, 0xFF}
+#define ORANGISH    CLITERAL(Color){0xFD, 0x8D, 0x14, 0xFF}
+#define REDISH    CLITERAL(Color){0xC5, 0x16, 0x05, 0xFF}
+#define YELLOWISH CLITERAL(Color){0xFF, 0xE1, 0x7B, 0xFF}
+
 #define WIDTH  BLOCK_SIZE * BLOCK_COUNT
 #define HEIGHT BLOCK_SIZE * BLOCK_COUNT
 
-typedef struct Rect
-{
-  float x;
-  float y;
-  float width;
-  float height;
-} Rect;
+// How far from the border to place the snake
+#define WRAP_DELAY 3
 
 typedef struct Player
 {
-    Rect player;
+    Rectangle player;
     int x_direction;
     int y_direction;
     double speed;
@@ -28,7 +29,7 @@ typedef struct Player
 
 typedef struct Food
 {
-    Rect entity;
+    Rectangle entity;
     double roundedness;
     Color color;
 } Food;
@@ -38,5 +39,9 @@ Player snake;
 Food food;
 
 void Initialize_game(void);
-bool DetectRectCollision(Rect a, Rect b);
+bool DetectRectCollision(Rectangle a, Rectangle b);
 void SnakeControl(double dt);
+void CollisionFunctions(void);
+void WrapSnakeBody(void);
+
+void DebugFunctions(void);
